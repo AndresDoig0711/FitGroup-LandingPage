434ce6b8e494288b105e25d64ddfbef1201f4fc1
ChumanY <89022180+ChumanY@users.noreply.github.com>
2022-04-25 18:16:33 -0500

    SCROLL SECTIONS ACTIVE LINK

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,25 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 //NICOLE
+
+/=============== SCROLL SECTIONS ACTIVE LINK ===============/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 50,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.navmenu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.navmenu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+//ALDAIR
